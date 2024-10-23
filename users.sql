f579f3a8bdc8b04c1d26c344206cb366437e334c
Patiphan Phengpao <misterpatipan123@gmail.com>
2024-10-24 06:36:52 +0700

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
